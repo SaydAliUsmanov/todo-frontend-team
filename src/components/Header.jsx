e395f8dd70b08562db60a14c63eea682659d5301
Magomed-Shabaev <shabaev95@gmail.com>
2021-11-26 16:29:12 +0300

    сделал первый вариант Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.css'
 
 function Header(props) {
+  const [search, setSearch] = useState('')
+
+  const handleSearch = (ev) => {
+    setSearch(ev.target.value)
+  }
+
   return (
-    <div className='bg-secondary w-100'>
-      go
+    <div className='header-wrapper bg-secondary w-100'>
+      <span className='header-logo'>E-Todos</span>
+      <input value={search} onChange={handleSearch} placeholder='Поиск по todo..' type="text" size='70'/>
+      <button className='btn btn-warning'>Добавить</button>
     </div>
   );
 }
