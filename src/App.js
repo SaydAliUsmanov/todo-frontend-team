3e047340c2f9df20f46029973920c7f47cbd65de
SaydAliUsmanov <usmanov.guno@mail.ru>
2021-11-26 18:57:46 +0300

    fixed merge Error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import Header from './components/Header';
+import Todos from './components/Todos/Todos';
+import Categories from './components/Caregories/Categories';
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Categories />
+      <Todos />
     </div>
   );
 }
